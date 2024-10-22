9b5d897936f9837ac659ba55e4f1bd2a85ebc6f4
Shin Jung Tae <shinjeongtae@gmail.com>
2024-10-22 22:30:40 +0900

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -5,6 +5,7 @@ apt-get upgrade -y
 apt-get install software-properties-common wget curl \
     python3-dev python3-pip python3-wheel python3-setuptools -y
 # install Python packages
+apt-get install git
 python3 -m pip install --upgrade pip
 pip3 install --user -r .devcontainer/requirements.txt
 # update CUDA Linux GPG repository key
@@ -17,8 +18,8 @@ mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/3bf863cc.pub
 add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/ /" -y
 apt-get update
-apt-get install libcudnn8=8.9.0.*-1+cuda11.8
-apt-get install libcudnn8-dev=8.9.0.*-1+cuda11.8
+apt-get install libcudnn8=8.9.0.*-1+cuda12.3
+apt-get install libcudnn8-dev=8.9.0.*-1+cuda12.3
 # install recommended packages
 apt-get install zlib1g g++ freeglut3-dev \
     libx11-dev libxmu-dev libxi-dev libglu1-mesa libglu1-mesa-dev libfreeimage-dev -y
