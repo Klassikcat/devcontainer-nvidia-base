80dd5bfbe060636ec31ed4245f46ed7e749d3716
Shin Jung Tae <shinjeongtae@gmail.com>
2025-03-01 22:59:55 +0900

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,6 +1,5 @@
-# update system
+# update apt library
 apt-get update
-apt-get upgrade -y
 # install Linux tools and Python 3
 apt-get install software-properties-common wget curl \
     python3-dev python3-pip python3-wheel python3-setuptools -y
