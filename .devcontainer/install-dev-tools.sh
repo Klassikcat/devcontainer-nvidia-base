707206335c3c5e0406d3e5d66ba4e6f13f88d89a
Shin Jung Tae <shinjeongtae@gmail.com>
2025-03-01 23:06:32 +0900

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,14 +2,13 @@
 apt-get update
 # install Linux tools and Python 3
 apt-get install software-properties-common wget curl \
-    python3-dev python3-pip python3-wheel python3-setuptools -y
+    python3-dev python3-pip python3-wheel python3-setuptools git gh -y
 # install Python packages
 python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install uv
-uv pip install --user -r .devcontainer/requirements.txt
-uv pip install marimo[sql]
+pip install -r .devcontainer/requirements.txt
 # clean up
 pip3 cache purge
 apt-get autoremove -y
