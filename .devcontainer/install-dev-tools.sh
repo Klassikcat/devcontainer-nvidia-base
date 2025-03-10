7a617af6d1a67896b1d26f7ad7e00ef3aa5b70e8
Shin Jung Tae <shinjeongtae@gmail.com>
2025-03-10 10:43:17 +0900

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -7,7 +7,7 @@ apt-get install software-properties-common wget curl \
 python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install --upgrade pip
-python3 -m pip install uv
+pip install uv
 uv pip install -r .devcontainer/requirements.txt
 # clean up
 apt-get install sudo
