0caf1b530c1f83ea7e041b3e26e4b918e1a6bd51
Shin Jung Tae <shinjeongtae@gmail.com>
2025-03-10 10:22:20 +0900

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -8,10 +8,9 @@ python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install uv
-pip install -r .devcontainer/requirements.txt
+uv pip install -r .devcontainer/requirements.txt
 # clean up
 apt-get install sudo
-python3 -m pip install uv 
 pip3 cache purge
 apt-get autoremove -y
 apt-get clean
