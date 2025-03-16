93904ffb005a9fe519943efe1bfe79c3c91e5bcf
Shin Jung Tae <shinjeongtae@gmail.com>
2025-03-17 02:01:56 +0900

    Update install-dev-tools.sh

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -6,7 +6,7 @@ apt-get install software-properties-common wget curl \
 # install Python packages
 python3 -m pip install --upgrade pip
 python3 -m venv .venv
-. .venv/bin/activate
+source .venv/bin/activate
 pip install uv
 uv pip install -r .devcontainer/requirements.txt
 # clean up
